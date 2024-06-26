0898d2ace7de78e716f9053343c02afd0c8448be
Microeinstein <microeinstein@inventati.org>
2024-06-27 01:17:55 +0200

    parse dumpsys packages

--- app-updater.py
+++ app-updater.py
@@ -13,6 +13,7 @@ from dataclasses import dataclass, field
 from io import StringIO
 from pathlib import Path
 from urllib.parse import urljoin
+from typing import Optional
 
 from adb import adb_commands, sign_cryptography
 from adb import usb_exceptions as usb_ex
@@ -25,6 +26,7 @@ def url2path(url: str) -> Path:
     if not m:
         raise RuntimeError(f"Unknown url: {url!r}")
     return Path(m[1])
+
 
 async def try_get_url(session, dir, url, new_ts: int = None):
     fpath = dir / url2path(url)
@@ -51,6 +53,41 @@ async def try_get_url(session, dir, url, new_ts: int = None):
                     break
                 file.write(chunk)
         return fpath
+
+
+def get_adb_device():
+    local_key = os.path.expanduser('~/.android/adbkey')
+    
+    if Path(local_key).exists():
+        signer = sign_cryptography.CryptographySigner(local_key)
+    else:
+        print("No adb local key (todo)")
+        return
+    
+    try:
+        device = adb_commands.AdbCommands()
+        device.ConnectDevice(rsa_keys=[signer])
+    except usb_ex.DeviceNotFoundError as ex:
+        print(ex.args[0])
+        return
+    
+    return device
+
+
+def chunk_read_lines(iter):
+    sio = StringIO()
+    for chunk in iter:
+        sio.write(chunk)
+        sio.seek(0)
+        line2 = None
+        for i, line in enumerate(sio):
+            if i > 0:
+                yield line2
+            line2 = line
+        sio = StringIO()
+        sio.write(line2)
+    sio.seek(0)
+    yield from sio
 
 
 @dataclass
@@ -105,54 +142,76 @@ class FDroidRepo:
                 return
 
 
-def adb_test():
-    local_key = os.path.expanduser('~/.android/adbkey')
-    
-    if Path(local_key).exists():
-        signer = sign_cryptography.CryptographySigner(local_key)
-    else:
-        print("No adb local key (todo)")
-        return
-    
-    try:
-        device = adb_commands.AdbCommands()
-        device.ConnectDevice(rsa_keys=[signer])
-    except usb_ex.DeviceNotFoundError as ex:
-        print(ex.args[0])
-        return
-    
-    for line in device.StreamingShell('dumpsys package packages'):
-        print(line)
-
-
 @dataclass
 class AndroidApp:
+    RGX_ATTR = re.compile(r"^[ \t]+([a-z]+)=(.*)$", re.I)
+    RGX_PKG  = re.compile(r"[^ ]+\{[^ ]+ ([^ ]+)\}.*$", re.I)
+    RGX_VER  = re.compile(r"([^ ]+).*$", re.I)
+    
     package: str
     version_code: str
     system: bool
-    installer: str
+    installer: Optional[str] = None
     
     @classmethod
-    def fetch_foreign_apps(cls):
-        ...
-    
+    def fetch_foreign_apps(cls, text_iter):
+        data = dict()
+        ignore = False
+        
+        for line in text_iter:
+            line = line.lower()
+            m = cls.RGX_ATTR.match(line)
+            if not m:
+                continue
+            
+            key, val = m.groups()
+            if key == 'pkg':
+                if data:
+                    if not ignore:
+                        yield cls(**data)
+                    data = dict()
+                ignore = val == 'null'
+                data['package'] = cls.RGX_PKG.match(val)[1] if not ignore else val
+            
+            elif key == 'versioncode':
+                data['version_code'] = cls.RGX_VER.match(val)[1]
+            
+            elif key == 'flags':
+                data['system'] = 'SYSTEM' in val
+            
+            elif key == 'installerpackagename':
+                data['installer'] = val
+        
+        if data and not ignore:
+            yield cls(**data)
 
 
 async def main(arg0, *args):
-    repos = list(FDroidRepo.read_from_backup())
+    # repos = list(FDroidRepo.read_from_backup())
     
-    print("Repositories:")
-    for r in repos:
-        print(f"  {r.name:<40s} {r.address}")
+    # print("Repositories:")
+    # for r in repos:
+    #     print(f"  {r.name:<40s} {r.address}")
     
-    print()
-    print("Updating repos...")
-    tasks = [r.update_repo() for r in repos]
-    await asyncio.gather(*tasks)
+    # print()
+    # print("Updating repos...")
+    # tasks = [r.update_repo() for r in repos]
+    # await asyncio.gather(*tasks)
     
     print()
     print("Getting packages...")
-    adb_test()
+    # phone = get_adb_device()
+    # if not phone:
+    #     return 1
+    # dumpsys = chunk_read_lines(phone.StreamingShell('dumpsys package packages'))
+    with open('dumpsys.txt', 'r') as dumpsys:
+        apps = list(AndroidApp.fetch_foreign_apps(dumpsys))
+        apps = sorted(
+            filter(lambda a: not a.system and a.installer, apps),
+            key=lambda a: (a.installer, a.package)
+        )
+        for a in apps:
+            print(f"  {a.package:<50} {a.version_code:<12} {a.system:<5} {a.installer}")
 
 
 if __name__ == '__main__':
